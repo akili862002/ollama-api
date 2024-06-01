8674ca6807becf121b8040007b950c61a45bbf12
Surtin <dung862002@gmail.com>
2024-06-01 18:48:00 +0700

    feat: stream chat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller('superai')
 export class AppController {
@@ -8,5 +9,14 @@ export class AppController {
   @Get('/chat/:model')
   chat(@Query('query') query: string, @Param('model') model = 'llama3') {
     return this.appService.chat(query, model as any);
+  }
+
+  @Get('/chat-stream/:model')
+  chatStream(
+    @Query('query') query: string,
+    @Param('model') model = 'llama3',
+    @Res() res: Response,
+  ) {
+    return this.appService.chatStream(query, model as any, res);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,7 @@ import * as nanoid from 'nanoid';
 import ollama, { Message } from 'ollama';
 import { Logger } from 'winston';
 import { createLogger } from './utils/logs.util';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
@@ -42,6 +43,49 @@ export class AppService {
       return {
         answer: response.message.content,
       };
+    } catch (error) {
+      console.error(error);
+      this.logger.error(error);
+
+      return {
+        error: error.message,
+      };
+    }
+  }
+
+  async chatStream(message: string, model: 'llama3' | 'phi3', res: Response) {
+    try {
+      const id = nanoid.nanoid();
+      const start = Date.now();
+      console.log('------------------------------');
+      console.log(`[${id}] ${dayjs().format('DD MMM YYYY - HH:mm:ss')}`);
+      console.log(`[${id}] Message: '${message}' (model: ${model})`);
+
+      this.logger.info(`[${id}] Message: '${message}' (model: ${model})`);
+
+      const messageItem: Message = {
+        role: 'user',
+        content: message,
+      };
+      const response = await ollama.chat({
+        model,
+        messages: [messageItem],
+        stream: true,
+      });
+
+      let answer = '';
+      this.logger.info(`[${id}] Answer: `);
+      for await (const part of response) {
+        process.stdout.write(part.message.content);
+        res.write(part.message.content);
+        answer += part.message.content;
+      }
+      res.end();
+
+      console.log(`[${id}] Ended. Duration: ${Date.now() - start}ms`);
+      console.log('------------------------------');
+
+      this.logger.info(`[${id}] Answer: '${answer}'`);
     } catch (error) {
       console.error(error);
       this.logger.error(error);
